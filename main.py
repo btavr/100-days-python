1e5f652ddb6e61352d6f3e2b5779e08cf0d0e4ee
btavr <brunotrodrigues98@gmail.com>
2024-06-05 11:15:34 +0100

    Day 8

---
+++ main.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+
+def greet(firstName, location):
+    print(f"Hello {firstName}")
+    print(f"How's the weather in {location}?")
+
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+greet("Bruno", "Austria")
